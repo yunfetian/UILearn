2e5c2fc797db4657e9c0ea61feb0a5af554da6cf
yunfetian <huanggang9098@163.com>
2015-07-26 08:30:11 +0900

    Initial Commit

---
+++ UILearn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UILearn
+//
+//  Created by 黄剛 on 2015/07/26.
+//  Copyright (c) 2015年 黄剛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
